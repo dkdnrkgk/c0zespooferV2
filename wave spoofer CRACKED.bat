7b6c2db722925b37a4cb98194aa8f8ac24a1245e
dkdnrkgk <domelandnoa@gmail.com>
2026-06-14 11:48:16 +0200

    Add files via upload

---
+++ wave spoofer CRACKED.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell -Command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('The program can''t start because VCRUNTIME140_1.dll is missing from your computer. Try reinstalling the program to fix this problem.', 'Wave Spoofer CRACKED.exe - System Error', 'OK', 'Error')"
+exit
